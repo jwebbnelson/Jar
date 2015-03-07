e7b130b01600ad52965b87210e0f9b654bdb577d
Jordan Nelson <nelsonjordan20@yahoo.com>
2015-03-07 16:07:16 -0700

    Login/SignUp ViewControllers
    Includes additional Framework classes for ParseUI

---
+++ JarApp/DefaultSettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  DefaultSettingsViewController.h
+//  JarApp
+//
+//  Created by Jordan Nelson on 3/7/15.
+//  Copyright (c) 2015 Jake Herrmann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DefaultSettingsViewController : UIViewController
+
+@end
