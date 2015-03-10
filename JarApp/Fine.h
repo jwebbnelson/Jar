f6a61073aa61ff6ae3d90cd6ff87604135aa03c9
Jordan Nelson <nelsonjordan20@yahoo.com>
2015-03-10 10:42:01 -0600

    trying git add .

---
+++ JarApp/Fine.h
@@ -0,0 +1,13 @@
+//
+//  Fine.h
+//  JarApp
+//
+//  Created by Jordan Nelson on 3/10/15.
+//  Copyright (c) 2015 Jake Herrmann. All rights reserved.
+//
+
+#import <Parse/Parse.h>
+
+@interface Fine : PFObject
+
+@end
---
+++ JarApp/Fine.m
@@ -0,0 +1,13 @@
+//
+//  Fine.m
+//  JarApp
+//
+//  Created by Jordan Nelson on 3/10/15.
+//  Copyright (c) 2015 Jake Herrmann. All rights reserved.
+//
+
+#import "Fine.h"
+
+@implementation Fine
+
+@end
