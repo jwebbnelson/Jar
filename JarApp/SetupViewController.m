a8a0ebc43a776fd78bfb4fe48e00423abfd36684
wagppinto <wagpinto@me.com>
2015-03-14 14:32:12 -0600

    Settings View Added
    The Settings View is added and the design is approved by the group.
    
    Signed-off-by: wagppinto <wagpinto@me.com>

---
+++ JarApp/SetupViewController.m
@@ -0,0 +1,37 @@
+//
+//  SetupViewController.m
+//  JarApp
+//
+//  Created by Wagner Pinto on 3/14/15.
+//  Copyright (c) 2015 Jake Herrmann. All rights reserved.
+//
+
+#import "SetupViewController.h"
+
+@interface SetupViewController ()
+
+@end
+
+@implementation SetupViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
