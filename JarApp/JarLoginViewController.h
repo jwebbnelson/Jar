9e2dd6331ec8c5617d3303256b6391f7c39300e1
Jordan Nelson <nelsonjordan20@yahoo.com>
2015-03-14 13:22:48 -0600

    Push through login/signup to shelf

---
+++ JarApp/JarLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  JarLoginViewController.h
+//  JarApp
+//
+//  Created by Jordan Nelson on 3/14/15.
+//  Copyright (c) 2015 Jake Herrmann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JarLoginViewController : UIViewController
+
+@end
---
+++ JarApp/JarSignUpViewController.h
@@ -0,0 +1,13 @@
+//
+//  JarSignUpViewController.h
+//  JarApp
+//
+//  Created by Jordan Nelson on 3/14/15.
+//  Copyright (c) 2015 Jake Herrmann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JarSignUpViewController : UIViewController
+
+@end
